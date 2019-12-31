490447640f5f6cad252e53e734d65898de9c6a9b
Lum Dërmaku <lum.dermaku@studentetu.uni-pr.edu>
2019-12-31 18:05:52 +0100

    Added JS for homepage

--- JS/home.js
+++ JS/home.js
@@ -1,4 +1,27 @@
 function slideNext()
 {
+    document.img1.src = images[4]; document.getElementById("p_1").innerHTML = desc[2]; document.getElementById("h2_1").innerHTML = headings[2];
+    document.img2.src = images[5]; document.getElementById("p_2").innerHTML = desc[3];  document.getElementById("h2_2").innerHTML = headings[3];
+    document.img3.src = images[6]; document.getElementById("p_3").innerHTML = desc[3]; document.getElementById("h2_3").innerHTML = headings[3];
+    document.img4.src = images[7]; document.getElementById("p_4").innerHTML = desc[3]; document.getElementById("h2_4").innerHTML = headings[3];
+}
+function slidePrev()
+{
+    document.img1.src = images[0]; document.getElementById("p_1").innerHTML = desc[0]; document.getElementById("h2_1").innerHTML = headings[0];
+    document.img2.src = images[1]; document.getElementById("p_2").innerHTML = desc[1];  document.getElementById("h2_2").innerHTML = headings[1];
+    document.img3.src = images[2]; document.getElementById("p_3").innerHTML = desc[1]; document.getElementById("h2_3").innerHTML = headings[1];
+    document.img4.src = images[3]; document.getElementById("p_4").innerHTML = desc[1]; document.getElementById("h2_4").innerHTML = headings[1];
+}
 
+function slide(a)
+{
+
+    if (a===1 )
+    {
+        slideNext()
+    }
+    else if (a===0)
+    {
+        slidePrev()
+    }
 }
---
+++ JS/home_content.js
@@ -0,0 +1,21 @@
+var images = [];
+images[0]="../IMAGES/img1.jpg"
+images[1]="../IMAGES/img2.jpg"
+images[2]="../IMAGES/img3.jpg"
+images[3]="../IMAGES/img4.jpg"
+images[4]="../IMAGES/img5.jpg"
+images[5]="../IMAGES/img6.jpg"
+images[6]="../IMAGES/img7.jpg"
+images[7]="../IMAGES/img8.jpg"
+
+var desc = [];
+desc[0] = "Justoid nonummy laoreet phasellen penatoque in antesque pellus elis eget tincidunt."
+desc[1] = "Inteligula congue id elis donec sce sagittis intes laoreet aenean  leo sem massawisi condisse leo sem ac. Tincidunt nibh quis dui fauctor."
+desc[2] = "Proin malesuada quam non ultricies consectetur. Sed pharetra libero dapibus, volutpat velit non, pellentesque lacus."
+desc[3] = "Sed quis volutpat mi. Mauris vel neque at mi ullamcorper accumsan tincidunt id tortor. Suspendisse vitae tincidunt ex, at congue magn."
+
+var headings = []
+headings[0] = "Lorem ipsum dolor"
+headings[1] = "Vivamuslibero Augue"
+headings[2] = "Duis aute irure dolor";
+headings[3] = "Donec Accumsan"
