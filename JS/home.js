a3a2f7bfcd3660cab914f63eff2cb894560d4acd
idealqoqaj <ideal.q@hotmail.com>
2020-01-03 00:57:09 +0100

    added more comments, fixed some code, added a new webpage.

--- JS/home.js
+++ JS/home.js
@@ -1,7 +1,8 @@
+// 10.1: Përdorimi i tagjeve elementare të JavaScript-ave
 function slideNext()// 19.8: Krijimi dhe përdorimi i funksioneve në JS me dhe pa parametra
 {
     // 19.7: Krijimi dhe përdorimi i objekteve në JS - ketu vetem perdorimi i objektit DOM
-    document.img1.src = images[4]; document.getElementById("p_1").innerHTML = desc[2]; document.getElementById("h2_1").innerHTML = headings[2];
+    document.img1.src = images[4]; document.getElementById("p_1").innerHTML = desc[2]; document.getElementById("h2_1").innerHTML = headings[2]; // 10.3: Përdorimi i JavaScript për manipulim me tekst si dheshfrytëzimi i getElementbyId
     document.img2.src = images[5]; document.getElementById("p_2").innerHTML = desc[3];  document.getElementById("h2_2").innerHTML = headings[3];
     document.img3.src = images[6]; document.getElementById("p_3").innerHTML = desc[3]; document.getElementById("h2_3").innerHTML = headings[3];
     document.img4.src = images[7]; document.getElementById("p_4").innerHTML = desc[3]; document.getElementById("h2_4").innerHTML = headings[3];
