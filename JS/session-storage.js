fda7e7bed901afe779b44a25f49b0f97e51e4de4
albiandragusha <albiandragusha@gmail.com>
2020-01-05 15:13:33 +0100

    more tasks

--- JS/session-storage.js
+++ JS/session-storage.js
@@ -1,16 +1,19 @@
 function save()
 {
-    if (typeof(Storage) !== "undefined")
+    var age = 20////19.9: Përdorimi i variablave lokale dhe globale – të përdoren me kuptim!;
+    if (typeof(Storage) !== "undefined") //10.10: Përdorimi i JS operatorëve (të paktën 4 raste)
     {
-        sessionStorage.setItem("inp", document.getElementById("fill").value)
+        sessionStorage.setItem("inp", document.getElementById("fill").value + ""+age)
         document.getElementById('result').innerHTML = sessionStorage.getItem("inp");
     }
 }
+var myName = "Lum";//19.9: Përdorimi i variablave lokale dhe globale – të përdoren me kuptim!; 19.5: Përdorimi i JS data tipeve (data types) në raste të ndryshme
+var specs = {} // 19.7: Krijimi dhe përdorimi i objekteve në JS;19.5: Përdorimi i JS data tipeve (data types) në raste të ndryshme
 window.addEventListener("Load",save,false) //10.2: Përdorimi i JavaScript për tekst (output), ngjarje (event)
 
 
 //Ne rast se e kemi nderru manifestin, duhet t'i tregojme page-it te behet update.
-var appCache = window.applicationCache;
+var appCache = window.applicationCache; //19.4: Përdorimi i JS variablave në raste të ndryshme ; 19.9: Përdorimi i variablave lokale dhe globale – të përdoren me kuptim!
 
 switch (appCache.status)
 {
@@ -32,7 +35,7 @@ switch (appCache.status)
 
 appCache.update();
 
-if (appCache.status == window.applicationCache.UPDATEREADY)
+if (appCache.status === window.applicationCache.UPDATEREADY)
 {
     appCache.swapCache();
 }
@@ -44,7 +47,8 @@ if (appCache.status == window.applicationCache.UPDATEREADY)
 window.addEventListener('load', function(e) {
 
     window.applicationCache.addEventListener('updateready', function(e) {
-        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+        if (window.applicationCache.status === window.applicationCache.UPDATEREADY) //19.10: Përdorimi i JS operatorëve (të paktën 4 raste)
+        {
             // Browseri e ka download cache te ri
             if (confirm('A new version of this site is available. Load it?')) {
                 window.location.reload();
